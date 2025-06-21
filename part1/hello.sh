e2187f56790223479f1c0568911cf03383d4b977
Deepanshu Mishra <deep270804@gmail.com>
2025-06-21 22:51:45 +0530

    Added part 01

---
+++ part1/hello.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+# This is script for Pending 
+echo "Judge: Kannon badi hi kamini cheez hn Patil"
+echo "Patil: Kamine aksar bura hi karte hn mere saath kiya kal aapke saath bhi karega" 
+
